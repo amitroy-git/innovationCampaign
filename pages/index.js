3328326e70d713d1802852cc7772fad2e6828354
amitroy-git <amitroy.790@gmail.com>
2019-12-14 12:07:29 +0530

    ui changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import factory from "../ethereum/factory";
+import {Card, Button} from 'semantic-ui-react';
+import Layout from '../components/Layout';
+import { Link } from "../routes";
 
 class CampaignIndex extends Component{
     static async getInitialProps(){
@@ -7,10 +10,36 @@ class CampaignIndex extends Component{
         
         return {campaigns};
     }
-
-    render(){
-    return<div>{this.props.campaigns}</div>
+    renderCampaigns(){
+        const items = this.props.campaigns.map(address=>{
+            return{
+                header: address,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                        <a>view campaigns</a>)
+                    </Link>),
+                fluid: true
+            };
+        });
+        return <Card.Group items={items}/>;
     }
-}
-
+    render(){
+    return(
+    <Layout>
+        <div>
+            <h3>Open Campaigns</h3>
+            <Link route='/campaigns/new'>
+            <a><Button
+                floated='right'
+                content='Create Campaign'
+                icon='add circle'
+                primary
+            /></a>
+            </Link>
+            {this.renderCampaigns()}
+        </div>
+    </Layout>
+    );
+};
+};
 export default CampaignIndex;
